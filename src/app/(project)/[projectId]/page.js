380ac7cab108d134f22520356260bf51d42d3533
Lara Salameh <hello@larasalameh.com>
2024-11-13 09:57:15 +1100

    meta desc tweaks

--- src/app/(project)/[projectId]/page.js
+++ src/app/(project)/[projectId]/page.js
@@ -11,7 +11,7 @@ export async function generateMetadata({ params }) {
       project?.title || "... oop no work here"
     }`,
     description:
-      "Lara Salameh is a senior UX designer & frontend developer. She loves working on projects that are good for people & planet. She loves creating immersive experiences that leave a positive impact on a person. Based in Melbourne, Australia, here's a selection of her recent work",
+      "Lara Salameh is a senior UX designer, frontend developer, an all-round creative technologist. She loves working on projects that are good for people & planet. She loves creating digital experiences that leave a positive impact on a person. Based in Melbourne, Australia, here's a selection of her recent work",
   };
 }
 
